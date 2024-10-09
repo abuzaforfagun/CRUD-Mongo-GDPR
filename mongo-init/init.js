ebd61f479de0ff3031453806744b8486d402863d
Fagun <mdabuzaforfagun@gmail.com>
2024-10-09 16:38:30 +0600

    Populate categories

--- mongo-init/init.js
+++ mongo-init/init.js
@@ -1,5 +1,24 @@
-db = db.getSiblingDB('secure-privacy');
 db.createCollection('users');
 db.createCollection('products');
+db.createCollection('categories');
 
-db.products.createIndex({ category: 1, created_at: -1 })
+db.products.createIndex({ "category.id": 1, created_at: -1 })
+
+db.categories.insertMany([
+    {
+        "_id": ObjectId(),
+        "Name": "Grocery"
+    },
+    {
+        "_id": ObjectId(),
+        "Name": "Electronics"
+    },
+    {
+        "_id": ObjectId(),
+        "Name": "Clothing"
+    },
+    {
+        "_id": ObjectId(),
+        "Name": "Books"
+    }
+]);
