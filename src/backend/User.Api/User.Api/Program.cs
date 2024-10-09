9005aedd729f34df77519269c3528638626d341f
Fagun <mdabuzaforfagun@gmail.com>
2024-10-09 13:58:08 +0600

    add dependency injection

--- src/backend/User.Api/User.Api/Program.cs
+++ src/backend/User.Api/User.Api/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using User.Api.Configurations;
+using User.Api.Repositories;
+using User.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<CryptographyConfig>(builder.Configuration.GetSection("Encryption"));
+
+
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDb"));
+var databaseSettings = builder.Configuration.GetSection("MongoDb").Get<MongoDbSettings>();
+var mongoClient = new MongoClient(databaseSettings!.ConnectionString);
+builder.Services.AddSingleton<IMongoClient>(mongoClient);
+builder.Services.AddSingleton(serviceProvider =>
+{
+    var settings = serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return mongoClient.GetDatabase(settings.DatabaseName);
+});
+
+
+builder.Services.AddScoped<ICryptographyService, CryptographyService>();
+builder.Services.AddScoped<IUserRepository, UsersRepository>();
+builder.Services.AddScoped<IUsersService, UsersService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
