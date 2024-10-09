2eca06203016d098faa9a236d977cf66ac2aa6fa
Fagun <mdabuzaforfagun@gmail.com>
2024-10-09 16:38:49 +0600

    dependency injection

--- src/backend/User.Api/Products.Api/Program.cs
+++ src/backend/User.Api/Products.Api/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Products.Api.Configurations;
+using Products.Api.Repositories;
+using Products.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDb"));
+var databaseSettings = builder.Configuration.GetSection("MongoDb").Get<MongoDbSettings>();
+var mongoClient = new MongoClient(databaseSettings!.ConnectionString);
+builder.Services.AddSingleton<IMongoClient>(mongoClient);
+builder.Services.AddSingleton(serviceProvider =>
+{
+    var settings = serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return mongoClient.GetDatabase(settings.DatabaseName);
+});
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductsService, ProductsService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
