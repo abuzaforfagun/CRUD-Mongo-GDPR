cd9e36a353235c58b2d0bc522b6befb5cc3d64c8
Fagun <mdabuzaforfagun@gmail.com>
2024-10-10 06:00:52 +0600

    send data from parent component

--- src/client/src/app/users/user-list/user-list.component.ts
+++ src/client/src/app/users/user-list/user-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { UserState } from '../ngrx/user.reducer';
+import { selectAllUsers } from '../ngrx/user.selectors';
+import * as UserActions from '../ngrx/user.actions';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
+  users$: Observable<any[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<UserState>) {
+    this.users$ = this.store.select(selectAllUsers);
   }
 
+  ngOnInit(): void {
+    this.store.dispatch(UserActions.loadUsers());
+
+    this.users$.subscribe((d) => console.log(d));
+  }
 }
--- src/client/src/app/users/user-table/user-table.component.ts
+++ src/client/src/app/users/user-table/user-table.component.ts
@@ -1,23 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { UserState } from '../ngrx/user.reducer';
-import { selectAllUsers } from '../ngrx/user.selectors';
-import * as UserActions from '../ngrx/user.actions';
+import { Component, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.scss'],
 })
-export class UserTableComponent implements OnInit {
-  users$: Observable<any[]>;
-
-  constructor(private store: Store<UserState>) {
-    this.users$ = this.store.select(selectAllUsers);
-  }
-
-  ngOnInit(): void {
-    this.store.dispatch(UserActions.loadUsers());
-  }
+export class UserTableComponent {
+  @Input() users: any[] = [];
 }
